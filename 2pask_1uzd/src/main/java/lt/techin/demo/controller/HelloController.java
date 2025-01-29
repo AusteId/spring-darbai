de9c7e75cb211f1ab5d11344e089fd81c7cc2dec
AusteId <auste.idaite@gmail.com>
2025-01-29 11:09:21 +0200

    Add first task

---
+++ 2pask_1uzd/src/main/java/lt/techin/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package lt.techin.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+//  @GetMapping("/hello")
+//  public ResponseEntity<String> printText() {
+//    return ResponseEntity.ok("Hello, Spring Web!");
+//  }
+
+  @GetMapping("/hello")
+  public String printText() {
+    return "Hello, Spring Web!";
+  }
+}
--- 2pask_1uzd/src/test/java/lt/techin/demo/DemoApplicationTests.java
+++ 2pask_1uzd/src/test/java/lt/techin/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package lt.techin.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
