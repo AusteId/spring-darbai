78ad2764040fcd2c6937d6fffacf23014a1955dc
AusteId <auste.idaite@gmail.com>
2025-02-03 15:15:38 +0200

    Add incomplete 4.1 task

---
+++ 4.1_uzd/src/main/java/lt/techin/demo/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package lt.techin.demo.repository;
+
+import lt.techin.demo.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+  boolean existsByTitle(String title);
+
+  boolean existsByDirector(String director);
+
+}
