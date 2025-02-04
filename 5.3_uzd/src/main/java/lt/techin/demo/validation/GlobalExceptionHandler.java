a5478c073d3d8e346de6d5499cbdbc5d9536377a
AusteId <auste.idaite@gmail.com>
2025-02-04 18:23:45 +0400

    Add incomplete 5.3 task

--- 5.3_uzd/src/main/java/lt/techin/demo/validation/GlobalExceptionHandler.java
+++ 5.3_uzd/src/main/java/lt/techin/demo/validation/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package lt.techin.demo.validation;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,20 @@ public class GlobalExceptionHandler {
     ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),
             error.getDefaultMessage())
     );
+
+    return ResponseEntity.badRequest().body(errors);
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<Map<String, String>> handleValidationErrors(ConstraintViolationException ex) {
+
+    // Serializes to JSON, using Jackson
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getConstraintViolations().forEach(violation -> {
+      errors.put(violation.getPropertyPath().toString(),
+              violation.getMessage());
+    });
 
     return ResponseEntity.badRequest().body(errors);
   }
