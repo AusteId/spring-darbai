6497694cfd0d75dd34a25a7359dec4cd0670c9fd
AusteId <auste.idaite@gmail.com>
2025-02-10 13:59:03 +0200

    Add incomplete 7.1 task

---
+++ 7.1_uzd/src/main/java/lt/techin/demo/controller/UserController.java
@@ -0,0 +1,6 @@
+package lt.techin.demo.controller;
+
+public class UserController {
+
+
+}
--- 7.1_uzd/src/main/java/lt/techin/demo/model/Role.java
+++ 7.1_uzd/src/main/java/lt/techin/demo/model/Role.java
@@ -1,6 +1,33 @@
 package lt.techin.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
 
+  private String name;
+
+  public Role() {
+  }
+
+  public Role(String name) {
+    this.name = name;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
